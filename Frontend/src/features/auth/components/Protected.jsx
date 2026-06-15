3d1c0b44deec2c922970ce39c0c2913263c1a41f
Surya Pratap Singh <suryachaudhary.8449@gmail.com>
2026-06-15 16:07:11 +0530

    fix: Protected role check

--- Frontend/src/features/auth/components/Protected.jsx
+++ Frontend/src/features/auth/components/Protected.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router";
 
-const Protected = ({children,role='buyer'}) => {
-    const user=useSelector((state)=>state.auth.user);
-    const loading   =useSelector((state)=>state.auth.loading);
-    if(loading){
+const Protected = ({ children, role = null }) => {
+    const user = useSelector((state) => state.auth.user);
+    const loading = useSelector((state) => state.auth.loading);
+    
+    if (loading) {
         return <h1>Loading...</h1>;
     }
-    if(!user){
+    if (!user) {
         return <Navigate to="/login" />;
     }
-    if(user.role!==role){
+    if (role && user.role !== role) {
         return <Navigate to="/" />;
     }
     return children;
