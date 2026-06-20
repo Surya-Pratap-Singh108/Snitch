5ad8049457cd357f0e6154c14c22124ed3795257
Surya Pratap Singh <suryachaudhary.8449@gmail.com>
2026-06-20 23:17:21 +0530

    fix: loading state in App.jsx

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -4,14 +4,15 @@ import { routes } from './app.routes.jsx'
 import { useSelector } from 'react-redux'
 import { useAuth } from '../features/auth/hook/useAuth.js';
 import { useEffect } from 'react';
-// App.jsx mein
-if (loading) return <div className="h-screen w-full flex items-center justify-center bg-zinc-950 text-amber-500 text-2xl">Loading...</div>
+
 
 function App() {
   const {handleGetMe}=useAuth();
+  const loading = useSelector((state) => state.auth.loading);
   useEffect(() => {
     handleGetMe();
   }, []);
+  if (loading) return <div className="h-screen w-full flex items-center justify-center bg-zinc-950 text-amber-500 text-2xl">Loading...</div>
   return (
     <>
       <RouterProvider router={routes} />
